3d9e058f42addb94f7d1694758422b3b3d1dcbee
salmonte91 <stevenalmonte4@gmail.com>
2023-01-25 19:58:42 -0500

    delete route complete

--- views/index.jsx
+++ views/index.jsx
@@ -5,12 +5,6 @@ function Index({ breads }) {
   return (
     <Default>
       <h2>Index Page</h2>
-      <div className="newButton">
-        <a href="/breads/new">
-          <button>Add a new bread</button>
-        </a>
-      </div>
-
       {/* <p>I have {breads[0].name} bread!</p> */}
       {/* This is a JSX comment. */}
       <ul>
@@ -22,6 +16,11 @@ function Index({ breads }) {
           );
         })}
       </ul>
+      <div className="newButton">
+        <a href="/breads/new">
+          <button>Add a new bread</button>
+        </a>
+      </div>
     </Default>
   );
 }
