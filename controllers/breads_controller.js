56aedfef7ee6e49c6fc360df2e37665c4d7a1a6f
salmonte91 <stevenalmonte4@gmail.com>
2023-02-01 20:14:08 -0500

    made changes for the  edit section

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -3,12 +3,29 @@ const breads = express.Router();
 const Bread = require("../models/bread.js");
 
 // INDEX
-breads.get("/", (req, res) => {
-  res.render("index", {
-    breads: Bread,
+breads.get("/", async (req, res) => {
+  Bread.find().then((foundBreads) => {
+    console.log(foundBreads)
+    res.render("index", {
+      breads: foundBreads,
+      title: "Index Page",
+    });
   });
-  // res.send(Bread)
 });
+
+// CREATE
+breads.post('/', async (req, res) => {
+  if(!req.body.image) {
+      req.body.image = undefined 
+  }
+  if(req.body.hasGluten === 'on') {
+    req.body.hasGluten = true
+  } else {
+    req.body.hasGluten = false
+  }
+  await Bread.create(req.body)
+  res.redirect('/breads')
+})
 
 // NEW
 breads.get("/new", (req, res) => {
@@ -16,55 +33,52 @@ breads.get("/new", (req, res) => {
 });
 
 // EDIT
-breads.get("/:indexArray/edit", (req, res) => {
-  res.render("edit", {
-    bread: Bread[req.params.indexArray],
-    index: req.params.indexArray,
-  });
-});
+breads.get('/:id/edit', (req, res) => {
+  Bread.findById(req.params.id) 
+    .then(foundBread => { 
+      res.render('edit', {
+        bread: foundBread 
+      })
+    })
+})
+
 
 // SHOW
-breads.get("/:arrayIndex", (req, res) => {
-  if (Bread[req.params.arrayIndex]) {
-    res.render("Show", {
-      bread: Bread[req.params.arrayIndex],
-      index: req.params.arrayIndex,
-    });
-  } else {
-    res.render("404");
-  }
-});
+breads.get('/:id', (req, res) => {
+  Bread.findById(req.params.id)
+    .then(foundBread => {
+      res.render('show', {
+        bread: foundBread
+      })
+    })
+    .catch(err => {
+      res.send('404')
+    })
+})
 
-// CREATE
-breads.post("/", (req, res) => {
-  if (!req.body.image) {
-    req.body.image =
-      "https://images.unsplash.com/photo-1517686469429-8bdb88b9f907?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80";
-  }
-  if (req.body.hasGluten === "on") {
-    req.body.hasGluten = true;
-  } else {
-    req.body.hasGluten = false;
-  }
-  Bread.push(req.body);
-  res.redirect("/breads");
-});
 
 // UPDATE
-breads.put("/:arrayIndex", (req, res) => {
-  if (req.body.hasGluten === "on") {
-    req.body.hasGluten = true;
+breads.put('/:id', (req, res) => {
+  if(req.body.hasGluten === 'on'){
+    req.body.hasGluten = true
   } else {
-    req.body.hasGluten = false;
+    req.body.hasGluten = false
   }
-  Bread[req.params.arrayIndex] = req.body;
-  res.redirect(`/breads/${req.params.arrayIndex}`);
-});
+  Bread.findByIdAndUpdate(req.params.id, req.body, { new: true }) 
+    .then(updatedBread => {
+      console.log(updatedBread) 
+      res.redirect(`/breads/${req.params.id}`) 
+    })
+})
+
 
 // DELETE
-breads.delete("/:indexArray", (req, res) => {
-  Bread.splice(req.params.indexArray, 1);
-  res.status(303).redirect("/breads");
-});
+breads.delete('/:id', (req, res) => {
+  Bread.findByIdAndDelete(req.params.id) 
+    .then(deletedBread => { 
+      res.status(303).redirect('/breads')
+    })
+})
+
 
 module.exports = breads;
--- views/show.jsx
+++ views/show.jsx
@@ -16,11 +16,11 @@ function Show({ bread, index }) {
       <li>
         <a href="/breads">Go home</a>
       </li>
-      <a href={`/breads/${index}/edit`}>
+      <a href={`/breads/${bread.id}/edit`}>
         <button>Edit</button>
       </a>
 
-      <form action={`/breads/${index}?_method=DELETE`} method="POST">
+      <form action={`/breads/${bread.id}?_method=DELETE`} method="POST">
         <input type="submit" value="DELETE" />
       </form>
     </Default>
